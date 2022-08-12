b9f71969e04dad9eee4bcf6d90ff6961add3c522
yinjindong <yinjindong@newrank.cn>
2022-08-12 08:59:45 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,58 @@
+import fs from "fs";
+import path from "path";
+import inquirer from "inquirer";
+import { envMap } from "./static.js";
+
+const ENV_PATH = path.resolve(process.cwd(), ".env.development.local");
+const ENV_FS = fs.readFileSync(ENV_PATH, "utf-8");
+
+const handleFile = (file, envMode) => {
+  const obj = Object.create(null);
+  file
+    .toString()
+    .split("\n")
+    .forEach(function (line) {
+      const keyValueArr = line.split("=");
+      const [key, val = ""] = keyValueArr;
+      if (!!key) {
+        obj[key] = val;
+      }
+    });
+  if (!obj[envMode]) {
+    throw new Error(
+      "在.env.development.local文件中找不到环境地址信息。\nPS: 测试、预发布、生产环境对应的键名分别为test、pre、production\n例如选择测试环境需要在文件中写入 test='http: test.dev.com'"
+    );
+  }
+  return { ...obj, REACT_APP_API_HOST: obj[envMode] };
+};
+
+const writeEnvFile = (handledObj) => {
+  let result = "";
+  Object.keys(handledObj).forEach((key) => {
+    result += `${key}=${handledObj[key]}\n`;
+  });
+  fs.writeFileSync(ENV_PATH, result);
+};
+
+const checkEnv = () => {
+  inquirer
+    .prompt([
+      {
+        name: "env",
+        type: "list",
+        message: "选择要切换的环境",
+        choices: Object.keys(envMap),
+        default: ["测试环境"],
+      },
+    ])
+    .then((res) => {
+      const env = envMap[res.env];
+      const handledObject = handleFile(ENV_FS, env);
+      writeEnvFile(handledObject);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
+
+export default checkEnv;
---
+++ static.js
@@ -0,0 +1,6 @@
+const envMap = {
+  测试环境: "test",
+  预发布: "pre",
+  生产环境: "production",
+};
+module.exports = { envMap };
